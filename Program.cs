ec00a00aa798de7e5b59314733cedae327845389
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-03 20:11:09 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@ namespace EmployeeWageNonStatic
 
             Console.WriteLine("***** Welcome to the employee wage computation Program for Static Methods. *****");
 
-            EmployeeWageStatic.Employee("BridgeLabz", 40, 15, 100);
-            EmployeeWageStatic.Employee("Microland", 40, 20, 150);
-            EmployeeWageStatic.Employee("Karvy", 20, 5, 50);
+            EmployeeWageStatic bridge = new EmployeeWageStatic("BridgeLabz", 20, 10, 50);
+            EmployeeWageStatic micro = new EmployeeWageStatic("MicroLand", 20, 10, 50);
+
+            bridge.ComputeEmpWage();
+            Console.WriteLine(bridge.Return());
+            micro.ComputeEmpWage();
+            Console.WriteLine(micro.Return());
+
         }
     }
 }
