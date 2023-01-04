f172823afdcf48ba8626b93db9efbf493045ae1c
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-04 23:13:51 +0530

    Interface Approach

---
+++ InterfaceEmpWage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public interface Iemployeewage
+    {
+        public void addCompanies(string Company, int wagePerHour, int maxdays, int maxhours);
+
+        public void computeWage();
+    }
+}
