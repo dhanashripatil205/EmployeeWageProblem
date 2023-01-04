0a25edd525b92044f3893e7358bc04e5ad5c9944
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-05 01:19:11 +0530

    Using Interfaace Method

---
+++ EmpWageInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public interface EmpWageInterface
+    {
+        public void addCompanies(string Company, int wagePerHour, int maxdays, int maxhours);
+
+        public void EmpComputation();
+
+    }
+}
